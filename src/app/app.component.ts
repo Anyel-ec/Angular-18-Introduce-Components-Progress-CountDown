4f0217917e016b63730b1cac68f8c19b332b8f56
anyel-ec <paulangel826@gmail.com>
2024-06-05 16:44:44 -0500

    add progress bar and countdown components to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
+import { CountdownComponent } from './components/countdown/countdown.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProgressBarComponent, CountdownComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'components';
+
+  counterProgress: number = 0;
+  totalCountdown: number =5;
+
+  updateProgress($event:number){
+    this.counterProgress = (this.totalCountdown - $event)/this.totalCountdown *100;
+  }
+
+  countdownFinished(){
+    console.log("Countdown Finished");
+  }
+
 }
---
+++ src/app/components/countdown/countdown.component.ts
@@ -0,0 +1,67 @@
+import { Component, Input, OnInit, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './countdown.component.html',
+  styleUrl: './countdown.component.scss'
+})
+
+
+export class CountdownComponent implements OnInit, OnChanges {
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //console.log('Valor de init cambio a: ', changes['init'].currentValue);
+    this.startCounter();
+  }
+
+  ngOnInit(): void {
+    this.startCounter();
+  }
+
+  @Output() onDecrease = new EventEmitter<number>();
+  @Output() onComplete = new EventEmitter<void>();
+
+  @Input() init:number = 0;
+  public counter:number = 0;
+
+  private countDownTimeRef:any = null;
+
+
+
+  startCounter(){
+    if (this.init && this.init>0){
+      this.clearTimeOutRef()
+      this.counter = this.init;
+      this.doCountdown();
+    }
+  }
+
+  doCountdown(){
+    this.countDownTimeRef = setTimeout(()=>{
+      this.counter = this.counter - 1;
+      this.processCountdown();
+    }, 1000)
+  }
+
+  private clearTimeOutRef(){
+    if (this.countDownTimeRef){
+      clearTimeout(this.countDownTimeRef);
+      this.countDownTimeRef = null;
+    }
+  }
+
+  processCountdown(){
+    this.onDecrease.emit(this.counter);
+
+    //console.log("La cuenta va en: ", this.counter);
+    if(this.counter == 0){
+      this.onComplete.emit();
+      //console.log("La cuenta ha terminado");
+    }
+    else{
+      this.doCountdown();
+    }
+  }
+}
